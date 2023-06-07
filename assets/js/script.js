c9c0cd5cffaa27a5896df6dfefe4fe9dc2639432
todiane <>
2023-06-07 21:36:04 +0000

    add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,49 @@
-function runGame() {}
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-function checkAnswer() {}
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
-function calculateCorrectAnswer() {}
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
-function incrementScore() {}
+function runGame() {
 
-function incrementWrongAnswer() {}
+}
 
-function displayAdditionQuestion() {}
+function checkAnswer() {
 
-function displaySubtractQuestion() {}
+}
 
-function displayMultiplyQuestion() {}
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
